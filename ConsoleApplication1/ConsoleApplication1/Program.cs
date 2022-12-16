9cfd3319b477cfd115e919978f22e13c7e365e02
pradeepInnovex <pradeepinnovax@gmail.com>
2022-12-16 17:45:28 +0530

    Interface Example Updated

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    interface IEmployee
+    {
+
+        void Paid();
+    }
+
+    interface ITemporaryEmployee
+    {
+        void Paid();
+    }
+
+    class Employee : IEmployee, ITemporaryEmployee
+    {
+        public void Paid()
+        {
+            Console.WriteLine("Payed..");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+
+            e1.Paid();
+        }
+    }
+}
